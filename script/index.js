e2faad429e8feff87ad2a576202a82bf4a18211d
Ashik <ashikkhan757@gmail.coml>
2026-02-17 18:40:29 +0600

    added product details modal

--- script/index.js
+++ script/index.js
@@ -1,12 +1,91 @@
 const loadCategories = () => {
     fetch("https://fakestoreapi.com/products/categories")
-    .then(res => res.json())
-    .then(category => displayCategory(category));
+        .then(res => res.json())
+        .then(category => displayCategory(category));
 };
 
-const loadCategoryProduct = (id) => {
-    console.log(id);
-    
+const removeActive = () =>{
+    const categoriesButtons = document.querySelectorAll(".category-btn");
+    // console.log(categoriesButtons);
+    categoriesButtons.forEach(btn => btn.classList.remove("active"));
+
+}
+
+const loadCategoryProduct = (category) => {
+    const url = `https://fakestoreapi.com/products/category/${encodeURIComponent(category)}`; 
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            removeActive(); 
+            const clickBtn = document.getElementById(`btn-${category}`);
+            // console.log(clickBtn);
+             clickBtn.classList.add("active");
+            displayCategoryProduct(data)
+        });
+};
+
+const displayCategoryProduct = (products)=>{
+    const productContainer = document.getElementById("product-container");
+    productContainer.innerHTML = "";
+
+    products.forEach(product => {
+        // console.log(product);
+        const card = document.createElement("div");
+        card.innerHTML = `
+          <div
+        class="bg-white rounded-2xl shadow-sm overflow-hidden flex flex-col cursor-pointer hover:shadow-xl transition"
+      >
+        <!-- Image -->
+        <div class="bg-gray-100 p-6 flex justify-center items-center">
+          <img
+            src="${product.image}"
+            alt="Product"
+            class="h-60 w-full object-contain"
+          />
+        </div>
+
+        <!-- Card Body -->
+        <div class="p-5 flex flex-col flex-1">
+          <div class="flex items-center justify-between mb-3">
+            <span
+              class="bg-indigo-100 text-indigo-600 text-xs font-medium px-3 py-1 rounded-full"
+            >
+              ${product.category}
+            </span>
+
+            <div class="flex items-center text-sm text-gray-500">
+              <i class="fa-solid fa-star text-yellow-400 mr-1"></i>
+              ${product.rating.rate} <span class="ml-1">(${product.rating.count})</span>
+            </div>
+          </div>
+
+          <h3 class="font-semibold text-gray-800 text-lg truncate">
+            ${product.title}
+          </h3>
+
+          <p class="text-xl font-bold mt-2 text-gray-900">$${product.price}</p>
+
+          <!-- Buttons -->
+          <div class="flex gap-3 mt-auto">
+            <button onclick="my_modal_5.showModal()" class="flex-1 border border-gray-300 rounded-lg py-2 text-gray-600 hover:bg-gray-100 transition cursor-pointer"
+            >
+              <i class="fa-regular fa-eye mr-2"></i>
+              Details
+            </button>
+
+            <button
+              class="flex-1 bg-indigo-600 text-white rounded-lg py-2 hover:bg-indigo-700 transition cursor-pointer"
+            >
+              <i class="fa-solid fa-cart-shopping mr-2"></i>
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+        `;
+        productContainer.append(card);
+    });
+
 }
 
 const displayCategory = (categories) => {
@@ -15,17 +94,26 @@ const displayCategory = (categories) => {
 
     for (let category of categories){
         const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `
-        <button onclick="loadCategoryProduct('${category}')" class="btn btn-outline btn-primary rounded-3xl">${capitalize(category)}</button>
-        `;
-        categoryContainer.append(btnDiv);
-    }  
-}
+        const btn = document.createElement("button");
+
+        btn.className = "btn btn-outline btn-primary rounded-3xl category-btn";
+        btn.id = `btn-${category}`;
+
+        btn.textContent = capitalize(category);
+
+        btn.addEventListener("click", () =>{
+            loadCategoryProduct(category);
+        });
+
+        btnDiv.appendChild(btn);
+        categoryContainer.appendChild(btnDiv);
+    }
+};
+
 
 const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
+
 loadCategories();
-
-
